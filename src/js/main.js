8219bc6ed08e3e737146dc0c104bdedc7d3f4c67
irabasara <125157770+irabasara@users.noreply.github.com>
2023-05-19 20:40:16 +0300

    add markup and some css

--- src/js/main.js
+++ src/js/main.js
@@ -1,55 +1,78 @@
-import axios from 'axios';
+import Image from './pixabayAPI';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const buttonLoadMore = document.querySelector('.load-more');
 const searchForm = document.querySelector('#search-form');
 const searchButton = document.querySelector('button[type=submit]');
 const gallery = document.querySelector('.gallery');
-console.log(searchButton);
+
+const image = new Image();
+const lightbox = new SimpleLightbox('.gallery a');
 
 searchForm.addEventListener('submit', onSearchFormSubmit);
+buttonLoadMore.addEventListener('click', onLoadMoreClick);
 
-async function onSearchFormSubmit(event) {
+function onSearchFormSubmit(event) {
   event.preventDefault();
-  const name = event.currentTarget.elements.searchQuery.value;
-  await axios
-    .get(
-      `https://pixabay.com/api/?key=36518003-e50cc2d75c5794a64cca810ae&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
-    )
-    .then(resp => {
-      gallery.innerHTML = createMarkup(resp.data.hits);
+  image.query = event.currentTarget.elements.searchQuery.value;
+
+  if (image.query === '') {
+    return window.alert('ddtlbnt ;lugycgjvhkbj');
+  }
+  image.resetPage();
+  image
+    .fetchImages()
+    .then(({ hits }) => {
+      gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+    })
+    .catch(error => console.log(error));
+  clearMarkup();
+}
+function onLoadMoreClick() {
+  imagesApiService
+    .fetchImages()
+    .then(({ hits }) => {
+      gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
     })
     .catch(error => console.log(error));
 }
 
+function clearMarkup() {
+  gallery.innerHTML = '';
+}
+
 function createMarkup(arr) {
-  return arr.map(
-    ({
-      webformatURL,
-      // largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      return `
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
     <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width=300 height=200/>
+  <a href='${largeImageURL}' class="gallery__link"><img src="${webformatURL}" alt="${tags}" loading="lazy" width=300 height=200/></a>
   <div class="info">
     <p class="info-item">
-      <b>Likes : </b>${likes}
+      <b>Likes</b>${likes}
     </p>
     <p class="info-item">
-      <b>Views : </b>${views}
+      <b>Views</b>${views}
     </p>
     <p class="info-item">
-      <b>Comments : </b>${comments}
+      <b>Comments</b>${comments}
     </p>
     <p class="info-item">
-      <b>Downloads : </b>${downloads}
+      <b>Downloads</b>${downloads}
     </p>
   </div>
 </div>`;
-    }
-  );
+      }
+    )
+    .join('');
 }
