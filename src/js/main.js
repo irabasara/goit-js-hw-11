5f1840a3eb51e681729f1297b9d28bf4adc84c14
irabasara <125157770+irabasara@users.noreply.github.com>
2023-05-22 15:05:15 +0300

    import-export and infinity scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,16 @@
 import Image from './pixabayAPI';
+import createMarkup from './markup';
+import refs from './refs';
+import onLoad from './observer';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Report } from 'notiflix';
 
-const buttonLoadMore = document.querySelector('.load-more');
-const searchForm = document.querySelector('#search-form');
-// const searchButton = document.querySelector('button[type=submit]');
-const gallery = document.querySelector('.gallery');
-// buttonLoadMore.style.visibility = 'hidden';
-
 const image = new Image();
 const lightbox = new SimpleLightbox('.gallery a');
 
-searchForm.addEventListener('submit', onSearchFormSubmit);
-buttonLoadMore.addEventListener('click', onLoadMoreClick);
+refs.searchForm.addEventListener('submit', onSearchFormSubmit);
+// buttonLoadMore.addEventListener('click', onLoadMoreClick);
 
 function onSearchFormSubmit(event) {
   event.preventDefault();
@@ -23,89 +20,59 @@ function onSearchFormSubmit(event) {
   image
     .fetchImages()
     .then(({ hits }) => {
-      btnNothidden();
-
       if (hits.length === 0 || image.query === '') {
-        btnIshidden();
         return Report.info(
           'INFO',
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
 
-      gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+      refs.gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+      observer.observe(refs.target);
       lightbox.refresh();
     })
     .catch(error => console.log(error));
-
   clearMarkup();
 }
 
-function onLoadMoreClick() {
-  image
-    .fetchImages()
-    .then(({ hits, totalHits }) => {
-      btnNothidden();
-      if (image.perPage >= totalHits) {
-        btnIshidden();
-        return Report.info(
-          'INFO',
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
+// function onLoadMoreClick() {
+//   image
+//     .fetchImages()
+//     .then(({ hits, totalHits }) => {
+//       if (image.perPage >= totalHits) {
+//         return Report.info(
+//           'INFO',
+//           "We're sorry, but you've reached the end of search results."
+//         );
+//       }
 
-      gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+//       gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
 
-      image.incrementHits(hits);
-    })
-    .catch(error => console.log(error));
-  btnNothidden();
-}
+//       image.incrementHits(hits);
+//     })
+//     .catch(error => console.log(error));
+// }
 
 function clearMarkup() {
-  gallery.innerHTML = '';
+  refs.gallery.innerHTML = '';
 }
 
-function createMarkup(arr) {
-  return arr
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-    <div class="photo-card">
-  <a href='${largeImageURL}' class="gallery__link"><img src="${webformatURL}" alt="${tags}" loading="lazy" width=300 height=200/></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>${downloads}
-    </p>
-  </div>
-</div>`;
-      }
-    )
-    .join('');
-}
+// function btnNothidden() {
+//   buttonLoadMore.style.visibility = 'visible';
+// }
 
-function btnNothidden() {
-  buttonLoadMore.style.visibility = 'visible';
-}
+// function btnIshidden() {
+//   buttonLoadMore.style.visibility = 'hidden';
+// }
+// btnIshidden();
 
-function btnIshidden() {
-  buttonLoadMore.style.visibility = 'hidden';
-}
-btnIshidden();
+let options = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(onLoad, options);
+// document.addEventListener('scroll', onScroll);
+
+// function onScroll() {}
